2a9ca66886bdf3cd7be6ebf6e850c0d83a3a18f6
Неволин Сергей <91075961+Jhin444444@users.noreply.github.com>
2022-09-26 17:15:57 +0300

    Add files via upload

---
+++ Разработка ПП  курс 2/Проект1/Проект1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Проект1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Удаляем из надписи Label2 ненужный текст
+            label2.Text = "";
+        }
+        // Обработчик щелчка по кнопке
+        private void button1_Click(object sender, EventArgs e)
+        {// Если поле для ввода не пустое:
+            if (textBox1.Text != "")
+                label2.Text = "Здравствуйте, " + textBox1.Text + "!";
+        }
+    }
+}
---
+++ Разработка ПП  курс 2/Проект2/Проект2/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Проект2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double k1, k2, k3;
+            try
+            {
+                k1 = Convert.ToDouble(textBox1.Text);
+                k2 = Convert.ToDouble(textBox2.Text);
+                k3 = k1 + k2;
+                textBox3.Text = k3.ToString("N");
+            }
+            catch
+            {if ((textBox1.Text == "") || (textBox2.Text == ""))
+                    MessageBox.Show("Введите данные!", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox1.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Вы действительно хотите выйти?", "Выход", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
